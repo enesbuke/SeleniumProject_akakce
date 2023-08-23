993f4edd2b668a4052570733afafcd63126b8c6c
Enes <enes.buke12@gmail.com>
2023-08-23 23:09:59 +0300

    Task Original Master ENes

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -4,15 +4,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BaseDriver {
 
     public static WebDriver driver;
     static {
 
+        Logger logger=Logger.getLogger("");
+        logger.setLevel(Level.SEVERE);
+
         driver=new ChromeDriver();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         driver.get("https://www.akakce.com");
 
     }
