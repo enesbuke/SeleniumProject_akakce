624cd2487207d090d474e75cb2adf154c44284d3
Enes <enes.buke12@gmail.com>
2023-08-24 17:54:33 +0300

    Commit Original Task

--- src/TestCase.java
+++ src/TestCase.java
@@ -1,18 +1,14 @@
 import Utility.BaseDriver;
-
 import Utility.Func;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 public class TestCase extends BaseDriver {
     @Test
-    public void Test1 () {
-        //aCreateAndAcVerifTest
+    public void Test1() {
 
         // Hesap açma butonuna tıklar
         WebElement newAccount = driver.findElement(By.xpath("//a[text()='Hesap Aç']"));
@@ -65,64 +61,139 @@ public class TestCase extends BaseDriver {
         month.selectByValue("4");
         year.selectByVisibleText("1998");
 
-
         //Kullanım koşullarını onayla .
-        WebElement usageAgreement = driver.findElement(By.xpath("//input[@type='checkbox'][@id=\"rnufpca\"]"));
+        WebElement usageAgreement = driver.findElement(By.xpath("//input[@type='checkbox'][@id='rnufpca']"));
         usageAgreement.click();
 
-        WebElement submit = driver.findElement(By.xpath("//input[@type='submit'][@value=\"Hesap aç\"]"));
+        WebElement submit = driver.findElement(By.xpath("//input[@type='submit'][@value='Hesap aç']"));
         submit.click();
 
+        //Kullanıcı ismi kontrol ediliyor
+        WebElement checkName = driver.findElement(By.xpath("//a[text()='Can']"));
+        System.out.println(checkName.getText());
+        Assert.assertTrue("Kullanıcı bulunamadı",checkName.getText().contains("Can"));
+        Func.Wait(2);
 
-        try {
+        WebElement hMenu = driver.findElement(By.xpath("//a[@title='Hesabım']"));
+        hMenu.click();
+        Func.Wait(1);
 
-            // Kullanıcı hesabı daha önce kayıt olmuş mu kontrol ediyor !
-            WebElement accountCreationControl = driver.findElement(By.xpath("//div[@class='m-c']"));
-            if (!accountCreationControl.isDisplayed()) {
-                System.out.println("Bu Email ile Başka bir hesap var !!!");
-            }
-        } catch (Exception ignored) {
+        WebElement exit=driver.findElement(By.xpath("//a[text()='Çık']"));
+        exit.click();
+        Func.Wait(1);
 
-        }
-
-        try {
-            // Kullanıcı isminin Can oldugunu kontrol ediyor
-            WebElement checkName = driver.findElement(By.cssSelector("[rel='nofollow'][href='/akakcem/']"));
-            Assert.assertEquals("Can", checkName.getText());
-        } catch (Exception ignored) {
-
-        }
     }
+    @Test
+    public void Test2(){
+        Func.Wait(3);
+        // Hesap açma butonuna tıklar
+        WebElement newAccount = driver.findElement(By.xpath("//a[text()='Hesap Aç']"));
+        newAccount.click();
 
-@Test
-    public void Test2 (){
-    //bLogoutTest2
+        //İsim (1),soyisim(2) ,email (3),tekrar email(4), şifre(5) ,tekrar şifre(6) girişi
 
-    WebElement hMenu = driver.findElement(By.xpath("//a[@title='Hesabım']"));
-    Func.Wait(2);
-    new Actions(driver)
-            .moveToElement(hMenu)
-            .build()
-            .perform();
+        //(1)
+        WebElement name = driver.findElement(By.xpath("//input[@type='text'][@name='rnufn']"));
+        name.sendKeys("Can");
+        //(2)
+        WebElement lastName = driver.findElement(By.xpath("//input[@type='text'][@name='rnufs']"));
+        lastName.sendKeys("Cen");
+        //(3)
+        WebElement email = driver.findElement(By.xpath("//input[@type='email'][@name='rnufe1']"));
+        email.sendKeys("sulmoyarza@gufum.com"); // Tek kullanımlık mail
+        //(4)
+        WebElement tryEmail = driver.findElement(By.xpath("//input[@type='email'][@name='rnufe2']"));
+        tryEmail.sendKeys("sulmoyarza@gufum.com");
+        //(5)
+        WebElement password = driver.findElement(By.xpath("//input[@type='password'][@name='rnufp1']"));
+        password.sendKeys("Can123Can");
+        //(6)
+        WebElement password2 = driver.findElement(By.xpath("//input[@type='password'][@name='rnufp2']"));
+        password2.sendKeys("Can123Can");
 
-    Func.Wait(2);
-    try {
+        //Cinsiyet seçimi
+        WebElement gender = driver.findElement(By.xpath("//input[@type='radio'][@name='rngc'][@id='rngm']"));
+        gender.click();
 
-    WebElement cikis=driver.findElement(By.xpath("//a[text()='Çık']"));
-    Func.Wait(2);
+        // İl ve İlçe Girişi
+        WebElement city = driver.findElement(By.xpath("//option[text()='Afyon']"));
+        city.click();
 
-    cikis.click();
-    Func.Wait(1);
+        WebElement county = driver.findElement(By.xpath("//option[text()='Çobanlar']"));
+        county.click();
 
-        Assert.assertFalse("Çıkış yapılamadı",cikis.isSelected());
-    }catch (Exception e) {
-        System.out.println("Button'a tıklamadı");
+
+        // Dogum Tarihi girişi .
+        WebElement webDay = driver.findElement(By.id("bd"));
+        WebElement webMonth = driver.findElement(By.id("bm"));
+        WebElement webYear = driver.findElement(By.id("by"));
+
+        Select day = new Select(webDay);
+        Select month = new Select(webMonth);
+        Select year = new Select(webYear);
+
+        day.selectByVisibleText("16");
+        month.selectByValue("4");
+        year.selectByVisibleText("1998");
+
+
+        //Kullanım koşullarını onayla .
+        WebElement usageAgreement = driver.findElement(By.xpath("//input[@type='checkbox'][@id='rnufpca']"));
+        usageAgreement.click();
+
+        WebElement submit = driver.findElement(By.xpath("//input[@type='submit'][@value='Hesap aç']"));
+        submit.click();
+
+        Func.Wait(2);
+
+        // Kullanıcı hesabı daha önce kayıt olmuş mu kontrol ediyor !
+        WebElement accountCreationControl = driver.findElement(By.cssSelector("div[class='alertX t2']"));
+        Assert.assertTrue("Bu eMail başka bir hasaba ait",accountCreationControl.getText().contains("zaten"));
+
+        WebElement okButton=driver.findElement(By.xpath("//button[@class='bt_v8 line_v8']"));
+        okButton.click();
+        Func.Wait(1);
+
+        WebElement akakce=driver.findElement(By.id("H_m_l_v8"));
+        akakce.click();
+        Func.Wait(1);
+
+        WebElement hMenu = driver.findElement(By.xpath("//a[text()='Giriş Yap']"));
+        hMenu.click();
+        Func.Wait(1);
+
+        WebElement emailA = driver.findElement(By.cssSelector("[id='life']"));
+        emailA.sendKeys("sulmoyarza@gufum.com");
+        Func.Wait(1);
+
+        WebElement passwordA = driver.findElement(By.cssSelector("[id='lifp']"));
+        passwordA.sendKeys("Can123Can");
+        Func.Wait(1);
+
+        WebElement Button = driver.findElement(By.cssSelector("[id='lfb']"));
+        Button.click();
     }
-        }
 
     @Test
-    public void Test3() {
-        //cLoginTest3
+    public void Test3(){
+
+        //  //bLogoutTest2
+        Func.Wait(2);
+        WebElement hMenu = driver.findElement(By.xpath("//a[@title='Hesabım']"));
+        hMenu.click();
+
+        WebElement exit=driver.findElement(By.xpath("//a[text()='Çık']"));
+        exit.click();
+        Func.Wait(1);
+
+        WebElement test=driver.findElement(By.xpath("(//a[text()='Giriş Yap'])[1]"));
+
+        Assert.assertTrue("Çıkış yapılamadı", test.isEnabled());
+    }
+
+    @Test
+
+    public void Test4(){
 
         WebElement girisYap = driver.findElement(By.cssSelector("[id='H_rl_v8'] a+a"));
         girisYap.click();
@@ -138,33 +209,16 @@ public class TestCase extends BaseDriver {
         Button.click();
         Func.Wait(2);
 
-        try {
 
-            WebElement errorW3 = driver.findElement(By.xpath("//div[@class=\"alertX t2\"]"));
+        WebElement hMenu = driver.findElement(By.xpath("//a[text()='Hesap Aç']"));
+        Assert.assertTrue("YANLIŞ ŞİRRE GİRİŞİ",hMenu.isEnabled());
 
-            if (!errorW3.isDisplayed()) {
-                System.out.println("Eposta veya Şifreniz hatalı");
-
-
-            }
-        } catch (Exception e) {
-
-
-        }
     }
     @Test
-    public void Test4(){
-        //dOrderListTest4
+    public void Test5(){
 
-        //Sipariş listesi giriş yapma
-        WebElement hMenu = driver.findElement(By.xpath("//div[@id='HM_v8']/a"));
-        new Actions(driver)
-                .moveToElement(hMenu)
-                .build()
-                .perform();
-
-
-        WebElement hesabim = driver.findElement(By.xpath("//a[text()='Hesabım']"));
+        Func.Wait(2);
+        WebElement hesabim=driver.findElement(By.xpath("//*[text()='Can'] "));
         hesabim.click();
         Func.Wait(1);
 
@@ -177,31 +231,35 @@ public class TestCase extends BaseDriver {
 
 
     }
-@Test
-public void Test5(){
-    //Mesajlarima Giris
-    Func.Wait(1);
-    WebElement MyMessages= driver.findElement(By.xpath("//a[text()='Mesajlarım']"));
-    MyMessages.click();
 
-    //Mesaj Kutusunun bos oldugunun kontrolu
-    WebElement confirmationText = driver.findElement(By.xpath("//div[@class='wbb_v8']"));
-    Assert.assertTrue("Listelenecek mesaj bulunamadı", confirmationText.getText().contains("Listelenecek mesaj bulunamadı"));
-    System.out.println("Mesajlarim = " + confirmationText.getText());
-
-
-}
     @Test
     public void Test6(){
 
-       //Hesap silme
+        Func.Wait(2);
+        WebElement hesabim=driver.findElement(By.xpath("//*[text()='Can'] "));
+        hesabim.click();
         Func.Wait(1);
-        WebElement webMenu=driver.findElement(By.xpath("//*[@title='Hesabım']"));
-        webMenu.click();
+
+        //Mesajlarima Giris
+        Func.Wait(1);
+        WebElement MyMessages= driver.findElement(By.xpath("//a[text()='Mesajlarım']"));
+        MyMessages.click();
+
+        //Mesaj Kutusunun bos oldugunun kontrolu
+        WebElement confirmationText = driver.findElement(By.xpath("//div[@class='wbb_v8']"));
+        Assert.assertTrue("Listelenecek mesaj bulunamadı", confirmationText.getText().contains("Listelenecek mesaj bulunamadı"));
+        System.out.println("Mesajlarim = " + confirmationText.getText());
 
 
-        WebElement btnHesabim=driver.findElement(By.xpath("//*[text()='Hesabım']"));
-        btnHesabim.click();
+    }
+
+    @Test
+    public void Test7(){
+
+        Func.Wait(2);
+        WebElement hesabim=driver.findElement(By.xpath("//*[text()='Can'] "));
+        hesabim.click();
+        Func.Wait(1);
 
         Func.Wait(1);
         WebElement hesapsil=driver.findElement(By.xpath("//*[text()='Hesabımı Sil']"));
@@ -214,21 +272,6 @@ public void Test5(){
 
         WebElement btnHesabimiSil=driver.findElement(By.xpath("//*[@id='u']"));
         btnHesabimiSil.click();
-
-        try {
-            WebElement passwordError = driver.findElement(By.xpath("//p[text()='Mevcut şifrenizi doğru girdiğinizden emin olun.']"));
-
-            if (!passwordError.isDisplayed()) {
-                System.out.println("Geçersiz Şifre");
-            }
-        }catch (NoSuchElementException e) {
-
-        }
-
-//        Func.Wait(2);
-//        String passwordCheck="JAtW-mZyb4.A-q";
-//        Assert.assertTrue("Geçersiz şifre ! ", password2.getText().contains(passwordCheck));
-
 
         WaitAndClose();
     }
